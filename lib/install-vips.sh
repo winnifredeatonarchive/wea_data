d971251e07dfe138c9bf3aec2fc285001a9dc27c
Joey Takeda <joey.takeda@gmail.com>
2019-04-15 18:48:20 -0700

    Adding checks for lib

--- lib/install-vips.sh
+++ lib/install-vips.sh
@@ -4,8 +4,18 @@
 # Taken from the ruby vips repo
 # https://github.com/libvips/ruby-vips/blob/master/.travis.yml
 
-cd lib;
-tar xf vips-8.7.4.tar.gz
-cd vips-8.7.4
+set -e
+
+# do we already have the correct vips built? early exit if yes
+# we could check the configure params as well I guess
+if [ -d "$HOME/vips/bin" ]; then
+		echo "Using cached directory"
+		exit 0
+fi
+
+rm -rf $HOME/vips
+wget $vips_site/v$version/vips-$version.tar.gz
+tar xf vips-$version.tar.gz
+cd vips-$version
 CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 ./configure --prefix=$HOME/vips $*
 make && make install
