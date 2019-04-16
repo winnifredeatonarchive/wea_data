035aa6276342649068a19c4b99540a06ab15e4cc
Joey Takeda <joey.takeda@gmail.com>
2019-04-15 18:37:51 -0700

    Fixing script

--- lib/install-vips.sh
+++ lib/install-vips.sh
@@ -4,7 +4,7 @@
 # Taken from the ruby vips repo
 # https://github.com/libvips/ruby-vips/blob/master/.travis.yml
 
-tar xf vips-8.7.4.tar.gz
-cd vips-8.7.4
+tar xf lib/vips-8.7.4.tar.gz
+cd lib/vips-8.7.4
 CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 ./configure --prefix=$HOME/vips $*
 make && make install
