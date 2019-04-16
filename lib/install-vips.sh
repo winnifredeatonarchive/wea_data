e845d1d5b2935b6b4c890b15ab61f760a2a9c2fc
Joey Takeda <joey.takeda@gmail.com>
2019-04-15 18:43:11 -0700

    Fixing paths with tar

--- lib/install-vips.sh
+++ lib/install-vips.sh
@@ -4,7 +4,8 @@
 # Taken from the ruby vips repo
 # https://github.com/libvips/ruby-vips/blob/master/.travis.yml
 
-tar xf lib/vips-8.7.4.tar.gz
-cd lib/vips-8.7.4
-CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 ./configure --prefix=$HOME/vips $*
-make && make install
+cd lib;
+tar xf vips-8.7.4.tar.gz
+cd vips-8.7.4
+#CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 ./configure --prefix=$HOME/vips $*
+#make && make install
