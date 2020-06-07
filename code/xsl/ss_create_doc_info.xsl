57af9a8b75e4dddb7ffb04f61650d7bf983b618a
Joey Takeda <joeytakeda@users.noreply.github.com>
2020-06-06 20:26:47 -0700

    Initial work on doc sorting stuff. Not finished yet

--- code/xsl/ss_create_doc_info.xsl
+++ code/xsl/ss_create_doc_info.xsl
@@ -5,6 +5,7 @@
     xmlns:xs="http://www.w3.org/2001/XMLSchema"
     xmlns:xd="http://www.oxygenxml.com/ns/doc/xsl"
     xmlns:map="http://www.w3.org/2005/xpath-functions"
+    xmlns:wea="https://github.com/wearchive/ns/1.0"
     xpath-default-namespace="http://www.w3.org/1999/xhtml"
     xmlns="http://www.w3.org/1999/xhtml"
     exclude-result-prefixes="#all"
@@ -20,11 +21,23 @@
     <xsl:include href="globals.xsl"/>
     
     
+    <xsl:variable name="dateMap" as="map(xs:string,xs:integer)">
+        <xsl:map>
+            <xsl:for-each select="$xhtmlDocs">
+                <xsl:sort select="wea:returnDate(.)"/>
+                <xsl:map-entry key="//html/@id/string(.)" select="position()"/>
+            </xsl:for-each>
+        </xsl:map>
+    </xsl:variable>
+    
+    
     <xsl:template match="/">
         <xsl:message>Creating document ajax frags...</xsl:message>
         <xsl:for-each select="$xhtmlDocs">
-            <xsl:result-document href="{$outDir || '/ajax/' || //html/@id || '.html'}">
+            <xsl:variable name="thisId" select="//html/@id"/>
+            <xsl:result-document href="{$outDir || '/ajax/' || $thisId || '.html'}">
                 <details open="open">
+                    <xsl:attribute name="data-dateOrder" select="$dateMap($thisId)"/>
                     <summary>More Info</summary>
                     <div>
                         <xsl:for-each-group select="//meta[matches(@class,'staticSearch\.(desc|date|bool)')]" group-by="@name">
@@ -32,7 +45,7 @@
                                 <div class="metadataLabel"><xsl:value-of select="current-grouping-key()"/></div>
                                 <div>
                                     <xsl:for-each select="current-group()">
-                                        <div>
+                                        <div data-filter="{@name}">
                                             <xsl:choose>
                                                 <xsl:when test="@data-link">
                                                     <a href="{@data-link}"><xsl:value-of select="@content"/></a>
@@ -52,6 +65,43 @@
             </xsl:result-document>
         </xsl:for-each>
     </xsl:template>
+    
+    <xsl:function name="wea:returnDate">
+        <xsl:param name="doc"/>
+        <xsl:variable name="dateMeta" select="$doc//meta[matches(@class,'staticSearch.date')][@name='Publication Date'][1]" as="element(meta)?"/>
+        <xsl:choose>
+            <xsl:when test="$dateMeta">
+                <xsl:choose>
+                    <xsl:when test="$dateMeta/@content castable as xs:date">
+                        <xsl:sequence select="xs:date($dateMeta/@content)"/>
+                    </xsl:when>
+                    <xsl:otherwise>
+                        <xsl:variable name="thisDocId" select="$doc//html/@id"/>
+                        <xsl:variable name="thisTeiDoc" select="$standaloneXml//tei:TEI[@xml:id=$thisDocId]"/>
+                        <xsl:variable name="thisPubDate" select="$thisTeiDoc//tei:sourceDesc/tei:bibl/tei:date/(@notBefore|@from|@when)[1]"/>
+                        <xsl:variable name="expandedDate" select="if ($thisPubDate) then wea:expandDate($thisPubDate) else xs:date('2020-01-01')"/>
+                        <xsl:sequence select="$expandedDate"/>
+                    </xsl:otherwise>
+                </xsl:choose>
+            </xsl:when>
+        </xsl:choose>
+    </xsl:function>
+    
+    <xsl:function name="wea:expandDate" as="xs:date">
+        <xsl:param name="date"/>
+        <xsl:variable name="tokens" select="tokenize($date,'-')"/>
+        <xsl:choose>
+            <xsl:when test="count($tokens) = 3">
+                <xsl:sequence select="xs:date($date)"/>
+            </xsl:when>
+            <xsl:when test="count($tokens) = 2">
+                <xsl:sequence select="xs:date($date || '-01')"/>
+            </xsl:when>
+            <xsl:when test="count($tokens) = 1">
+                <xsl:sequence select="xs:date($date || '-01-01')"/>
+            </xsl:when>
+        </xsl:choose>
+    </xsl:function>
     
     
 </xsl:stylesheet>
--- site/js/wea_search.js
+++ site/js/wea_search.js
@@ -1,65 +1,92 @@
-                var Sch;
-                window.addEventListener('load', 
-                
-                function(){
-                
-                Sch = new StaticSearch();
-                var filterBtn = document.getElementById('filterSearch');
-                filterBtn.addEventListener('click', function(e){
-                    document.getElementById('ssDoSearch').click();
-                });
-                var filters = document.querySelectorAll('.wea-ss-filters input');
-                filters.forEach(function(i){
-                    i.addEventListener('change',function(e){
-                        console.log('changed');
-                        if (filterBtn.disabled){
-                            filterBtn.removeAttribute('disabled');
-                        }
+let Sch;
+window.addEventListener('load', function () {
+    Sch = new StaticSearch();
+    var filterBtn = document.getElementById('filterSearch');
+    filterBtn.addEventListener('click', function (e) {
+        document.getElementById('ssDoSearch').click();
+    });
+    var filters = document.querySelectorAll('.wea-ss-filters input');
+    filters.forEach(function (i) {
+        i.addEventListener('change', function (e) {
+            console.log('changed');
+            if (filterBtn.disabled) {
+                filterBtn.removeAttribute('disabled');
+            }
+        });
+    });
+    var textInputs = document.querySelectorAll('.wea-ss-filters input[type="text"]');
+    textInputs.forEach(function (input) {
+        input.addEventListener('keydown', function (e) {
+            if (e.key == 'Enter') {
+                document.getElementById('ssDoSearch').click();
+            }
+        });
+    });
+    
+    Sch.searchFinishedHook = function () {
+        let thisResults = this.resultsDiv;
+        let resultObjs = thisResults.querySelectorAll('div');
+        let headers = this.resultsDiv.querySelectorAll('div > a');
+        let images = this.resultsDiv.querySelectorAll('img');
+        let hasKwic = thisResults.querySelectorAll(".kwic").length > 0;
+        images.forEach(function (img) {
+            img.classList.add('lazy');
+            img.setAttribute('data-src', img.src);
+            img.src = '';
+        });
+        lazyload();
+        filterBtn.setAttribute('disabled', 'disabled');
+        headers.forEach(function (head) {
+            let link = head.getAttribute('href');
+            fetch('ajax/' + link).then(function (file) {
+                /* Get the response as text */
+                return file.text();
+            }).then(function (frag) {
+                /*  And put the stuff in a nonce div and pass the inner HTML to it*/
+                var nonce = document.createElement('div');
+                nonce.innerHTML = frag;
+                head.after(nonce.firstElementChild)
+            });
+        });
+         let buttonDiv = document.createElement('div');
+         thisResults.insertBefore(buttonDiv, thisResults.firstElementChild);
+         /* JUST HIDE THE DIV FOR NOW */
+         buttonDiv.style.display='none';
+        ['score','date', 'title'].forEach(function(o){
+            let btn = document.createElement('button');
+            btn.setAttribute('id', o + 'Sort');
+            btn.innerHTML = o.charAt(0).toUpperCase() + o.slice(1);
+            if (hasKwic && o == 'score'){
+                btn.disabled='true';
+            }
+            if (!hasKwic && o=='score'){
+                return;
+            }
+            buttonDiv.appendChild(btn);
+            btn.addEventListener('click', function(){
+                let sortKeyArr = [];
+                if (o == 'score'){
+                    resultObjs.forEach(function(r){
+                        r.style.order = '';
                     });
-                });
-                
-                var textInputs = document.querySelectorAll('.wea-ss-filters input[type="text"]');
-                textInputs.forEach(function(input){
-                    input.addEventListener('keydown', function(e){
-                       if (e.key == 'Enter'){
-                           document.getElementById('ssDoSearch').click();
-                       } 
-                    }); 
-                });
-                
-                Sch.searchFinishedHook = function(){
-                let thisResults = this.resultsDiv;
-                let headers = this.resultsDiv.querySelectorAll('div > a');
-                let images = this.resultsDiv.querySelectorAll('img');
-                images.forEach(function(img){
-                   img.classList.add('lazy');
-                   img.setAttribute('data-src',img.src);
-                   img.src='';
-                });
-                lazyload();
-                filterBtn.setAttribute('disabled', 'disabled');
-                headers.forEach(function(head){
-                    let link = head.getAttribute('href');
-                fetch('ajax/' + link)
-                    .then(function(file){
-                     /* Get the response as text */
-                         return file.text();
-                     })
-                    .then(function(frag){
-                     /*  And put the stuff in a nonce div and pass the inner HTML to it*/
-                        var nonce = document.createElement('div');
-                        nonce.innerHTML = frag;
-                        head.after(nonce.firstElementChild)
+                } else if (o == 'date'){
+                    resultObjs.forEach(function(r){
+                        r.parentNode.style.order = r.querySelectorAll('details')[0].getAttribute('data-dateorder');
                     });
-                    
-                });
-                    
-                    
-                }
-              
+                } 
                 
-                
-                });
+            });
+        });
 
+    }
+});
 
-                 
+function getDate(r){
+    let date = r.querySelectorAll("div[data-filter='Publication Date']")[0];
+    if (date.length > 0){
+        
+    } else {
+        return new Date('2020-01-01');
+    }
+}
+
